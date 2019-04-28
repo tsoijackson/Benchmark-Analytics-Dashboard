cf3f6c884f240afa2a4613a284568267e8878673
Jackson Tsoi <tsoijackson2@gmail.com>
2019-04-28 14:08:26 -0700

    Update navbar.js

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import withStyles from "@material-ui/core/styles/withStyles";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-function Navbar(props) {
-    return (
-        <div>
-            <AppBar position="static" color="default">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit">
-                        Benchmarks Analytics Dashboard
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+const styles = {
+    root: {
+        backgroundColor: '#191d1e !important',
+        color: '#de5e5f'
+    }
 }
 
-export default Navbar;
+class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <AppBar position="static" className={this.props.classes.root}>
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit">
+                            Benchmarks Analytics Dashboard
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+
+
+}
+
+export default withStyles(styles)(Navbar);
